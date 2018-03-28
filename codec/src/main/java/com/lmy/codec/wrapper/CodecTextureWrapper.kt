9922da06cc1af67ceb3f026641270000bf1cd77e
李明艺 <lmyooyo@gmail.com>
2018-03-28 16:19:29 +0800

    编解码器缓冲区封装

---
+++ codec/src/main/java/com/lmy/codec/wrapper/CodecTextureWrapper.kt
@@ -0,0 +1,37 @@
+package com.lmy.codec.wrapper
+
+import android.opengl.EGLContext
+import android.view.Surface
+import com.lmy.codec.entity.Egl
+import com.lmy.codec.texture.impl.BaseTexture
+import com.lmy.codec.util.debug_e
+
+/**
+ * Created by lmyooyo@gmail.com on 2018/3/28.
+ */
+class CodecTextureWrapper(surface: Surface? = null,
+                          var eglContext: EGLContext? = null,
+                          var egl: Egl? = null,
+                          private var texture: BaseTexture? = null) : TextureWrapper() {
+    init {
+        if (null != surface) {
+            egl = Egl()
+            egl!!.initEGL(surface!!)
+            egl!!.makeCurrent()
+        } else {
+            debug_e("Egl create failed")
+        }
+    }
+
+    fun setFilter(texture: BaseTexture) {
+        this.texture = texture
+    }
+
+    fun drawTexture(transformMatrix: FloatArray) {
+        if (null == texture) {
+            debug_e("Render failed. Texture is null")
+            return
+        }
+        texture?.drawTexture(transformMatrix)
+    }
+}
