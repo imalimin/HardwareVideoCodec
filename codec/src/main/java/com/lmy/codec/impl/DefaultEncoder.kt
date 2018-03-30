af8c52c70c6a0a3912db6e40d90a2648fbbca888
李明艺 <lmyooyo@gmail.com>
2018-03-30 18:05:27 +0800

    抽象时间戳测量工具

--- codec/src/main/java/com/lmy/codec/impl/DefaultEncoder.kt
+++ codec/src/main/java/com/lmy/codec/impl/DefaultEncoder.kt
@@ -28,7 +28,8 @@ class DefaultEncoder(var parameter: Parameter,
                      private var codec: MediaCodec? = null,
                      private var format: MediaFormat? = null,
                      private var filter: BaseTexture? = null,
-                     private var mBufferInfo: MediaCodec.BufferInfo = MediaCodec.BufferInfo())
+                     private var mBufferInfo: MediaCodec.BufferInfo = MediaCodec.BufferInfo(),
+                     private var pTimer: PresentationTimer = PresentationTimer(parameter.video.fps))
     : Encoder {
 
     companion object {
@@ -42,8 +43,6 @@ class DefaultEncoder(var parameter: Parameter,
     private var mHandler: Handler? = null
     private val mEncodingSyn = Any()
     private var mEncoding = false
-    private var mTimestamp: Long = 0
-    private var mPresentationTimeUs: Long = 0
 
     private var onSampleListener: Encoder.OnSampleListener? = null
     override fun setOnSampleListener(listener: Encoder.OnSampleListener) {
@@ -107,6 +106,7 @@ class DefaultEncoder(var parameter: Parameter,
             debug_e("codec is null")
             return
         }
+        pTimer.reset()
         codec!!.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE)
         codecWrapper = CodecTextureWrapper(codec!!.createInputSurface(), cameraWrapper.egl!!.eglContext)
         filter = NormalTexture(cameraWrapper.getFrameTexture(), cameraWrapper.getDrawer())
@@ -125,12 +125,7 @@ class DefaultEncoder(var parameter: Parameter,
     }
 
     private fun encode() {
-        val timestamp = System.currentTimeMillis()
-        if (0L != mTimestamp)
-            mPresentationTimeUs += (timestamp - mTimestamp) * 1000
-        else
-            mPresentationTimeUs += 1000000 / parameter.video.fps
-        mTimestamp = timestamp
+        pTimer.record()
         codecWrapper?.egl?.makeCurrent()
         GLES20.glViewport(0, 0, parameter.video.width, parameter.video.height)
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
@@ -161,7 +156,7 @@ class DefaultEncoder(var parameter: Parameter,
                     if (null != data) {
                         val endOfStream = mBufferInfo.flags and MediaCodec.BUFFER_FLAG_END_OF_STREAM
                         if (endOfStream == 0) {
-                            mBufferInfo.presentationTimeUs = mPresentationTimeUs
+                            mBufferInfo.presentationTimeUs = pTimer.presentationTimeUs
                             onSampleListener?.onSample(mBufferInfo, data)
                         }
                         // 一定要记得释放
@@ -179,7 +174,7 @@ class DefaultEncoder(var parameter: Parameter,
 
     override fun start() {
         synchronized(mEncodingSyn) {
-            mTimestamp = 0
+            pTimer.start()
             mEncoding = true
         }
     }
@@ -198,5 +193,28 @@ class DefaultEncoder(var parameter: Parameter,
         pause()
         mHandler?.removeMessages(STOP)
         mHandler?.sendMessage(mHandler!!.obtainMessage(STOP, listener))
+    }
+
+    class PresentationTimer(var fps: Int,
+                            var presentationTimeUs: Long = 0,
+                            private var timestamp: Long = 0) {
+
+        fun start() {
+            timestamp = 0
+        }
+
+        fun record() {
+            val timeTmp = System.currentTimeMillis()
+            presentationTimeUs += if (0L != timestamp)
+                (timeTmp - timestamp) * 1000
+            else
+                1000000L / fps
+            timestamp = timeTmp
+        }
+
+        fun reset() {
+            presentationTimeUs = 0
+            timestamp = 0
+        }
     }
 }
