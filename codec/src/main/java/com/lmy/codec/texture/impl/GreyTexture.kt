c96c161445530de229fb0ebfe6876b8ea06b32e7
李明艺 <lmyooyo@gmail.com>
2018-03-30 18:27:15 +0800

    完善黑白滤镜

--- codec/src/main/java/com/lmy/codec/texture/impl/GreyTexture.kt
+++ codec/src/main/java/com/lmy/codec/texture/impl/GreyTexture.kt
@@ -1,92 +1,73 @@
 package com.lmy.codec.texture.impl
 
-import android.opengl.GLES11Ext
 import android.opengl.GLES20
 
 /**
  * Created by lmyooyo@gmail.com on 2018/3/27.
  */
-class GreyTexture(var inputTextureId: Int) : BaseTexture() {
+class GreyTexture(var inputTextureId: Int,
+                  var drawer: BaseFrameBufferTexture.GLDrawer) : BaseTexture() {
+
     companion object {
         private val VERTEX_SHADER = "" +
-                //顶点坐标
                 "attribute vec4 aPosition;\n" +
-                //纹理矩阵
-                "uniform mat4 uTextureMatrix;\n" +
-                //自己定义的纹理坐标
-                "attribute vec4 aTextureCoordinate;\n" +
-                //传给片段着色器的纹理坐标
+                "attribute vec2 aTextureCoord;\n" +
                 "varying vec2 vTextureCoord;\n" +
-                "void main()\n" +
-                "{\n" +
-                //根据自己定义的纹理坐标和纹理矩阵求取传给片段着色器的纹理坐标
-                "  vTextureCoord = (uTextureMatrix * aTextureCoordinate).xy;\n" +
-                "  gl_Position = aPosition;\n" +
-                "}\n"
+                "void main(){\n" +
+                "    gl_Position= aPosition;\n" +
+                "    vTextureCoord = aTextureCoord;\n" +
+                "}"
         private val FRAGMENT_SHADER = "" +
                 //使用外部纹理必须支持此扩展
-                "#extension GL_OES_EGL_image_external : require\n" +
                 "precision mediump float;\n" +
                 //外部纹理采样器
-                "uniform samplerExternalOES uTextureSampler;\n" +
-                "varying vec2 vTextureCoord;\n" +
+                "uniform sampler2D uTexture;\n" +
+                "varying mediump vec2 vTextureCoord;\n" +
                 "void main() \n" +
                 "{\n" +
                 //获取此纹理（预览图像）对应坐标的颜色值
-                "  vec4 vCameraColor = texture2D(uTextureSampler, vTextureCoord);\n" +
+                "  vec4 vCameraColor = texture2D(uTexture, vTextureCoord);\n" +
                 //求此颜色的灰度值
                 "  float fGrayColor = (0.3*vCameraColor.r + 0.59*vCameraColor.g + 0.11*vCameraColor.b);\n" +
                 //将此灰度值作为输出颜色的RGB值，这样就会变成黑白滤镜
                 "  gl_FragColor = vec4(fGrayColor, fGrayColor, fGrayColor, 1.0);\n" +
                 "}\n"
+        private val VERTICES_SCREEN = floatArrayOf(
+                0.0f, 1.0f,
+                0.0f, 0.0f,
+                1.0f, 0.0f,
+                1.0f, 1.0f)
     }
 
     private var aPositionLocation = 0
-    private var aTextureCoordLocation = 0
-    private var uTextureMatrixLocation = 0
-    private var uTextureSamplerLocation = 0
+    private var aTextureCoordinateLocation = 0
+    private var uTextureLocation = 0
 
     init {
+        verticesBuffer = createShapeVerticesBuffer(VERTICES_SCREEN)
+        createProgram()
+    }
+
+    private fun createProgram() {
         shaderProgram = createProgram(VERTEX_SHADER, FRAGMENT_SHADER)
+        aPositionLocation = getAttribLocation("aPosition")
+        uTextureLocation = getUniformLocation("uTexture")
+        aTextureCoordinateLocation = getAttribLocation("aTextureCoord")
     }
 
     override fun drawTexture(transformMatrix: FloatArray?) {
-        aPositionLocation = getPositionLocation()
-        aTextureCoordLocation = getTextureCoordinateLocation()
-        uTextureMatrixLocation = getTextureMatrixLocation()
-        uTextureSamplerLocation = getTextureSamplerLocation()
-
+        GLES20.glUseProgram(shaderProgram!!)
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
-        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, inputTextureId)
-        GLES20.glUniform1i(uTextureSamplerLocation, 0)
-        GLES20.glUniformMatrix4fv(uTextureMatrixLocation, 1, false, transformMatrix, 0)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, inputTextureId)
+        GLES20.glUniform1i(uTextureLocation, 0)
+        enableVertex(aPositionLocation, aTextureCoordinateLocation, buffer!!, verticesBuffer!!)
 
-        if (null != buffer) {
-            buffer!!.position(0)
-            GLES20.glEnableVertexAttribArray(aPositionLocation)
-            GLES20.glVertexAttribPointer(aPositionLocation, 2, GLES20.GL_FLOAT, false, 16, buffer)
+        drawer.draw()
 
-            buffer!!.position(2)
-            GLES20.glEnableVertexAttribArray(aTextureCoordLocation)
-            GLES20.glVertexAttribPointer(aTextureCoordLocation, 2, GLES20.GL_FLOAT, false, 16, buffer)
-
-            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
-        }
-    }
-
-    fun getPositionLocation(): Int {
-        return getAttribLocation("aPosition")
-    }
-
-    fun getTextureCoordinateLocation(): Int {
-        return getAttribLocation("aTextureCoordinate")
-    }
-
-    fun getTextureMatrixLocation(): Int {
-        return getUniformLocation("uTextureMatrix")
-    }
-
-    fun getTextureSamplerLocation(): Int {
-        return getUniformLocation("uTextureSampler")
+        GLES20.glFinish()
+        GLES20.glDisableVertexAttribArray(aPositionLocation)
+        GLES20.glDisableVertexAttribArray(aTextureCoordinateLocation)
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
+        GLES20.glUseProgram(0)
     }
 }
