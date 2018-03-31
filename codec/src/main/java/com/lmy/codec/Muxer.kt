917dbd242fc11855c57c4d748ba806a0dd2e6500
lmy <425078310@qq.com>
2018-03-31 13:57:15 +0800

    修改接口规范

--- codec/src/main/java/com/lmy/codec/Muxer.kt
+++ codec/src/main/java/com/lmy/codec/Muxer.kt
@@ -1,11 +1,15 @@
 package com.lmy.codec
 
+import android.media.MediaFormat
 import com.lmy.codec.entity.Sample
 
 /**
  * Created by lmyooyo@gmail.com on 2018/3/28.
  */
 interface Muxer {
-    fun write(sample: Sample)
+    fun addVideoTrack(format: MediaFormat)
+    fun addAudioTrack(format: MediaFormat)
+    fun writeVideoSample(sample: Sample)
+    fun writeAudioSample(sample: Sample)
     fun release()
 }
