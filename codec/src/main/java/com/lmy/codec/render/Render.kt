5b18c748f60b7ce71f3ce7a8345580d867be6ae4
limin <lmyooyo@gmail.com>
2018-04-07 20:37:01 +0800

    增加渲染后回调

--- codec/src/main/java/com/lmy/codec/render/Render.kt
+++ codec/src/main/java/com/lmy/codec/render/Render.kt
@@ -5,10 +5,15 @@ import android.graphics.SurfaceTexture
 /**
  * Created by lmyooyo@gmail.com on 2018/3/27.
  */
-interface Render : SurfaceTexture.OnFrameAvailableListener {
+interface Render {
+    fun onFrameAvailable(): Render
     fun draw()
     fun start(texture: SurfaceTexture, width: Int, height: Int)
     fun start(texture: SurfaceTexture, width: Int, height: Int, runnable: Runnable?)
     fun stop()
     fun release()
+    /**
+     * After render completed
+     */
+    fun afterRender(runnable: Runnable)
 }
