c156c922b868b57c1aa9e627e94b34967ef7020a
limin <lmyooyo@gmail.com>
2018-04-07 20:37:14 +0800

    完善软编逻辑

--- codec/src/main/java/com/lmy/codec/x264/X264Encoder.kt
+++ codec/src/main/java/com/lmy/codec/x264/X264Encoder.kt
@@ -1,11 +1,15 @@
 package com.lmy.codec.x264
 
 import android.media.MediaFormat
+import com.lmy.codec.util.debug_e
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 /**
  * Created by lmyooyo@gmail.com on 2018/4/3.
  */
-class X264Encoder(private var format: MediaFormat) {
+class X264Encoder(private var format: MediaFormat,
+                  var buffer: ByteBuffer? = null) {
     init {
         System.loadLibrary("x264")
         System.loadLibrary("codec")
@@ -14,6 +18,19 @@ class X264Encoder(private var format: MediaFormat) {
         setBitrate(format.getInteger(MediaFormat.KEY_BIT_RATE))
         setFrameFormat(FrameFormat.X264_CSP_RGB)
         setFps(format.getInteger(MediaFormat.KEY_FRAME_RATE))
+        buffer = ByteBuffer.allocate(720 * 480 * 3)
+        buffer?.order(ByteOrder.nativeOrder())
+    }
+
+    private fun createBuffer(size: Int): ByteArray {
+        debug_e("Create buffer($size)")
+        return buffer!!.array()
+    }
+
+    fun encode(src: ByteArray, srcSize: Int): Int {
+        buffer?.position()
+        buffer?.clear()
+        return encode(src, srcSize, buffer!!.array())
     }
 
     private external fun init()
