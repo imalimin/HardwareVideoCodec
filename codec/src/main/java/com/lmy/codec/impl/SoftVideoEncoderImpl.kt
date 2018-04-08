baecb1d7e8c829b6b6a20a8af0b27e1065ea3446
李明艺 <lmyooyo@gmail.com>
2018-04-08 18:48:37 +0800

    使用PBO

--- codec/src/main/java/com/lmy/codec/impl/SoftVideoEncoderImpl.kt
+++ codec/src/main/java/com/lmy/codec/impl/SoftVideoEncoderImpl.kt
@@ -1,9 +1,11 @@
 package com.lmy.codec.impl
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.graphics.SurfaceTexture
 import android.media.MediaCodec
 import android.media.MediaFormat
-import android.opengl.GLES20
+import android.opengl.GLES30
 import android.os.Build
 import android.os.Handler
 import android.os.HandlerThread
@@ -14,8 +16,9 @@ import com.lmy.codec.helper.CodecHelper
 import com.lmy.codec.util.debug_e
 import com.lmy.codec.wrapper.CameraTextureWrapper
 import com.lmy.codec.x264.X264Encoder
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
 import java.nio.ByteBuffer
-import java.nio.ByteOrder
 
 /**
  * Created by lmyooyo@gmail.com on 2018/4/3.
@@ -23,12 +26,14 @@ import java.nio.ByteOrder
 class SoftVideoEncoderImpl(var parameter: Parameter,
                            var cameraWrapper: CameraTextureWrapper,
                            var codec: X264Encoder? = null,
+                           private var pbos: IntArray = IntArray(PBO_COUNT),
                            private var format: MediaFormat = MediaFormat(),
                            private var srcBuffer: ByteBuffer? = null,
                            private var mBufferInfo: MediaCodec.BufferInfo = MediaCodec.BufferInfo(),
                            private var pTimer: VideoEncoderImpl.PresentationTimer = VideoEncoderImpl.PresentationTimer(parameter.video.fps)) : Encoder {
 
     companion object {
+        val PBO_COUNT = 2
         val HEADER: Array<Byte> = arrayOf(0, 0, 0, 1, 103, 100, 0, 30, -84, -46, 2, -48, -10, -102, -126, -125, 2, -125, 104, 80, -102, -128, 0, 0, 0, 1, 104, -18, 6, -30, -64)
         val INIT = 0x1
         val ENCODE = 0x2
@@ -45,6 +50,8 @@ class SoftVideoEncoderImpl(var parameter: Parameter,
     private val mEncodingSyn = Any()
     private var mEncoding = false
     private var mFrameCount = 0
+    private var index = 0
+    private var nextIndex = 1
 
     private var onSampleListener: Encoder.OnSampleListener? = null
     override fun setOnSampleListener(listener: Encoder.OnSampleListener) {
@@ -54,8 +61,7 @@ class SoftVideoEncoderImpl(var parameter: Parameter,
     init {
         initCodec()
         initThread()
-        srcBuffer = ByteBuffer.allocate(720 * 480 * 3)
-        srcBuffer?.order(ByteOrder.nativeOrder())
+        initPBOs()
         mHandler?.removeMessages(VideoEncoderImpl.INIT)
         mHandler?.sendEmptyMessage(VideoEncoderImpl.INIT)
     }
@@ -66,6 +72,18 @@ class SoftVideoEncoderImpl(var parameter: Parameter,
         codec?.setProfile("high")
         codec?.setLevel(31)
         codec?.start()
+    }
+
+    private fun initPBOs() {
+        val size = parameter.video.width * parameter.video.height * 4
+        pbos = IntArray(PBO_COUNT)
+        GLES30.glGenBuffers(PBO_COUNT, pbos, 0)
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, pbos[0])
+        GLES30.glBufferData(GLES30.GL_PIXEL_PACK_BUFFER, size, null, GLES30.GL_DYNAMIC_READ)
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, pbos[1])
+        GLES30.glBufferData(GLES30.GL_PIXEL_PACK_BUFFER, size, null, GLES30.GL_DYNAMIC_READ)
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, 0)
+        debug_e("initPBOs(" + pbos[0] + ", " + pbos[1] + ")")
     }
 
     private fun getOutFormat(): MediaFormat {
@@ -113,7 +131,17 @@ class SoftVideoEncoderImpl(var parameter: Parameter,
         pTimer.record()
         if (srcBuffer == null) return
         val time = System.currentTimeMillis()
-        val size = codec?.encode(srcBuffer!!.array(), srcBuffer!!.capacity())!!
+        val data: ByteArray
+        if (srcBuffer!!.hasArray()) {
+            data = srcBuffer!!.array()
+        } else {
+            data = ByteArray(srcBuffer!!.capacity())
+            srcBuffer!!.get(data)
+        }
+//        if (0 == mFrameCount % 24) {
+//            shotScreen(data, parameter.video.width, parameter.video.height)
+//        }
+        val size = codec?.encode(data, srcBuffer!!.capacity())!!
         if (0 == size) return
 //        if (1 == mFrameCount) {
 //            mBufferInfo.size = 31
@@ -140,24 +168,52 @@ class SoftVideoEncoderImpl(var parameter: Parameter,
     }
 
     private fun readPixels() {
-        if (0 != mFrameCount % 24) return
-        srcBuffer!!.clear()
-        srcBuffer!!.position(0)
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, GLES20.GL_NONE)
-//        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, cameraWrapper.getFrameBuffer())
-        GLES20.glReadPixels(0, 576, 720, 480, GLES20.GL_RGB,
-                GLES20.GL_UNSIGNED_BYTE, srcBuffer!!)
-        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, GLES20.GL_NONE)
+//        GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, cameraWrapper.getFrameBuffer())
+//        GLES30.glReadBuffer(GLES30.GL_FRONT);
+        //绑定到第一个PBO
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, pbos[index])
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            GLES30.glReadPixels(0, 576, parameter.video.width, parameter.video.height,
+                    GLES30.GL_RGBA, GLES30.GL_UNSIGNED_BYTE, 0)
+        }
+        //绑定到第二个PBO
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, pbos[nextIndex])
+        //glMapBufferRange会等待DMA传输完成，所以需要交替使用pbo
+        //映射内存
+        srcBuffer = GLES30.glMapBufferRange(GLES30.GL_PIXEL_PACK_BUFFER,
+                0, parameter.video.width * parameter.video.height * 4,
+                GLES30.GL_MAP_READ_BIT) as ByteBuffer
+        //解除映射
+        GLES30.glUnmapBuffer(GLES30.GL_PIXEL_PACK_BUFFER)
+        //解除绑定PBO
+        GLES30.glBindBuffer(GLES30.GL_PIXEL_PACK_BUFFER, 0)
+        GLES30.glBindFramebuffer(GLES30.GL_FRAMEBUFFER, 0)
+        //交换索引
+        index = (index + 1) % 2
+        nextIndex = (nextIndex + 1) % 2
+        if (null == srcBuffer) {
+            debug_e("PBO is null(${pbos[0]}, ${pbos[1]})")
+            return
+        }
         debug_e("buffer[${srcBuffer!![2000]}, ${srcBuffer!![2001]}, ${srcBuffer!![2002]}, ${srcBuffer!![2003]}]")
+    }
 
-//        val bitmap = Bitmap.createBitmap(bmpBuffer!!.array(), 720, 480, Bitmap.Config.RGB_565)
-//        if (null == bitmap) {
-//            debug_e("Bitmap is null")
-//            return
-//        }
-//        val out = FileOutputStream("/storage/emulated/0/000.jpg")
-//        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, out)
-//        debug_e("Saved!")
+
+    @Throws(FileNotFoundException::class)
+    private fun shotScreen(data: ByteArray, width: Int, height: Int) {
+        val dataTmp = IntArray(data.size / 4)
+        for (i in dataTmp.indices) {
+            dataTmp[i] = Color.argb(data[i * 4 + 3].toInt(), data[i * 4].toInt(),
+                    data[i * 4 + 1].toInt(), data[i * 4 + 2].toInt())
+        }
+        val bitmap = Bitmap.createBitmap(dataTmp, width, height, Bitmap.Config.ARGB_8888)
+        if (null == bitmap) {
+            debug_e("Bitmap is null")
+            return
+        }
+        val out = FileOutputStream("/storage/emulated/0/000.jpg")
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, out)
+        debug_e("Saved!")
     }
 
     override fun start() {
