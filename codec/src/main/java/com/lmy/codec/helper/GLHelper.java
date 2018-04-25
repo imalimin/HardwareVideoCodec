8f495570ea0ec9aef1bf090c1ebfd6be9b6c7004
李明艺 <lmyooyo@gmail.com>
2018-04-25 14:14:35 +0800

    增加glVersion方法

--- codec/src/main/java/com/lmy/codec/helper/GLHelper.java
+++ codec/src/main/java/com/lmy/codec/helper/GLHelper.java
@@ -1,5 +1,9 @@
 package com.lmy.codec.helper;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.pm.ConfigurationInfo;
+
 /**
  * Created by lmyooyo@gmail.com on 2018/4/25.
  */
@@ -11,4 +15,15 @@ public class GLHelper {
                                            int height,
                                            int format,
                                            int type);
+
+    /**
+     * @param context
+     * @return hex
+     */
+    public static int glVersion(Context context) {
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo info = am.getDeviceConfigurationInfo();
+        if (null == info) return 0;
+        return info.reqGlEsVersion;
+    }
 }
