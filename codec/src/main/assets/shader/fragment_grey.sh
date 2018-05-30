cdbef2deecd0240ce5f215263f820005d2d869b8
李明艺 <lmyooyo@gmail.com>
2018-05-30 11:40:05 +0800

    使用新的rbg混合比例

--- codec/src/main/assets/shader/fragment_grey.sh
+++ codec/src/main/assets/shader/fragment_grey.sh
@@ -1,13 +1,12 @@
 //使用外部纹理必须支持此扩展
-precision mediump float;
+precision highp float;
 //外部纹理采样器
 uniform sampler2D uTexture;
-varying mediump vec2 vTextureCoord;
+varying vec2 vTextureCoord;
+//const highp vec3 W = vec3(0.3, 0.59, 0.11);
+const highp vec3 W = vec3(0.2125, 0.7154, 0.0721);
 void main(){
-    //获取此纹理（预览图像）对应坐标的颜色值
-    vec4 vCameraColor = texture2D(uTexture, vTextureCoord);
-    //求此颜色的灰度值
-    float fGrayColor = (0.3*vCameraColor.r + 0.59*vCameraColor.g + 0.11*vCameraColor.b);
-    //将此灰度值作为输出颜色的RGB值，这样就会变成黑白滤镜
-    gl_FragColor = vec4(fGrayColor, fGrayColor, fGrayColor, 1.0);
+    lowp vec4 textureColor = texture2D(uTexture, vTextureCoord);
+    float luminance = dot(textureColor.rgb, W);
+    gl_FragColor = vec4(vec3(luminance), textureColor.a);
 }
