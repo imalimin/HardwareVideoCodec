2e3e97968b8569c3eb62d7c721c559254e3148a3
李明艺 <lmyooyo@gmail.com>
2018-06-13 20:14:28 +0800

    增加x265模块

--- codec/src/main/jni/Android.mk
+++ codec/src/main/jni/Android.mk
@@ -23,6 +23,11 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_MODULE := libx264
     LOCAL_SRC_FILES := lib/armeabi-v7a/libx264.so
     include $(PREBUILT_SHARED_LIBRARY)
+
+    include $(CLEAR_VARS)
+    LOCAL_MODULE := libx265
+    LOCAL_SRC_FILES := lib/armeabi-v7a/libx265.so
+    include $(PREBUILT_SHARED_LIBRARY)
 endif
 
 ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), x86 x86_64))
@@ -43,7 +48,7 @@ LOCAL_LDLIBS    := -lm -llog
 # -g 后面的一系列附加项目添加了才能使用 arm_neon.h 头文件
 
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    LOCAL_CFLAGS += -std=c99 -g -mfloat-abi=softfp -mfpu=neon -march=armv7-a -mtune=cortex-a8
+    LOCAL_CFLAGS += -mfloat-abi=softfp -mfpu=neon -march=armv7-a -mtune=cortex-a8
 endif
 
 # ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
@@ -51,7 +56,11 @@ endif
 # endif
 
 LOCAL_MODULE := codec
-LOCAL_SRC_FILES =: com_lmy_codec_x264_X264Encoder.c com_lmy_codec_helper_GLHelper.c
+LOCAL_SRC_FILES := com_lmy_codec_x264_X264Encoder.c \
+    com_lmy_codec_helper_GLHelper.c \
+    JNI_X265Encoder.cpp \
+    X265Encoder.cpp \
+
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     # 采用NEON优化技术
     LOCAL_ARM_NEON := true
@@ -63,5 +72,5 @@ endif
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_LDLIBS := -llog -lz -ljnigraphics -landroid -lm -pthread -lGLESv2
-LOCAL_SHARED_LIBRARIES := libyuv libx264
+LOCAL_SHARED_LIBRARIES := libyuv libx264 libx265
 include $(BUILD_SHARED_LIBRARY)
