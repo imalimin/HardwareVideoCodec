c5a93194efdef162d4de073cdeaf8d8be36a43db
李明艺 <lmyooyo@gmail.com>
2018-06-13 20:11:40 +0800

    增加x265编码器

---
+++ codec/src/main/jni/include/x265_config.h
@@ -0,0 +1,11 @@
+#ifndef X265_CONFIG_H
+#define X265_CONFIG_H
+
+/* Defines generated at build time */
+
+/* Incremented each time public API is changed, X265_BUILD is used as
+ * the shared library SONAME on platforms which support it. It also
+ * prevents linking against a different version of the static lib */
+#define X265_BUILD  0//${X265_BUILD}
+
+#endif
