406cb332fb693272ad2fe7e3fd4579e029e0bed9
李明艺 <lmyooyo@gmail.com>
2018-07-19 20:18:57 +0800

    支持更新纹理大小和裁剪

--- codec/src/main/java/com/lmy/codec/texture/impl/CameraTexture.kt
+++ codec/src/main/java/com/lmy/codec/texture/impl/CameraTexture.kt
@@ -10,6 +10,7 @@ import android.opengl.GLES11Ext
 import android.opengl.GLES20
 import com.lmy.codec.BaseApplication
 import com.lmy.codec.helper.AssetsHelper
+import com.lmy.codec.util.debug_e
 
 /**
  * Created by lmyooyo@gmail.com on 2018/3/29.
@@ -17,25 +18,33 @@ import com.lmy.codec.helper.AssetsHelper
 class CameraTexture(width: Int, height: Int,
                     textureId: Int) : BaseFrameBufferTexture(width, height, textureId) {
 
-    companion object {
-        private val CAMERA_TEXTURE_VERTICES = floatArrayOf(
-                0.0f, 0.0f,//LEFT,BOTTOM
-                1.0f, 0.0f,//RIGHT,BOTTOM
-                0.0f, 1.0f,//LEFT,TOP
-                1.0f, 1.0f//RIGHT,TOP
-        )
-    }
-
     private var aPositionLocation = 0
     private var uTextureLocation = 0
     private var aTextureCoordinateLocation = 0
     private var uTextureMatrix = 0
+    private val frameBufferLock = Any()
+    private val verticesBufferLock = Any()
 
     init {
-        verticesBuffer = createShapeVerticesBuffer(CAMERA_TEXTURE_VERTICES)
+        verticesBuffer = createShapeVerticesBuffer(getVertices(1f, 1f))
 
         createProgram()
         initFrameBuffer()
+    }
+
+    fun updateFrameBuffer(width: Int, height: Int) {
+        synchronized(frameBufferLock) {
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, frameBufferTexture!!)
+            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height,
+                    0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null)
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, GLES20.GL_NONE)
+        }
+    }
+
+    fun crop(cropRatioWidth: Float, cropRatioHeight: Float) {
+        synchronized(verticesBufferLock) {
+            verticesBuffer = createShapeVerticesBuffer(getVertices(cropRatioWidth, cropRatioHeight))
+        }
     }
 
     private fun createProgram() {
@@ -52,40 +61,39 @@ class CameraTexture(width: Int, height: Int,
             throw RuntimeException("TransformMatrix can not be null")
         synchronized(frameBufferLock) {
             GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer!!)
-            GLES20.glUseProgram(shaderProgram!!)
-            GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureId)
-            GLES20.glUniform1i(uTextureLocation, 0)
-            enableVertex(aPositionLocation, aTextureCoordinateLocation, buffer!!, verticesBuffer!!)
-            GLES20.glUniformMatrix4fv(uTextureMatrix, 1, false, transformMatrix, 0)
-
-            drawer.draw()
-
-            GLES20.glFinish()
-            GLES20.glDisableVertexAttribArray(aPositionLocation)
-            GLES20.glDisableVertexAttribArray(aTextureCoordinateLocation)
-            GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_NONE)
-            GLES20.glUseProgram(GLES20.GL_NONE)
-            GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, GLES20.GL_NONE)
         }
+        GLES20.glUseProgram(shaderProgram!!)
+        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, textureId)
+        GLES20.glUniform1i(uTextureLocation, 0)
+        synchronized(verticesBufferLock) {
+            enableVertex(aPositionLocation, aTextureCoordinateLocation, buffer!!, verticesBuffer!!)
+        }
+        GLES20.glUniformMatrix4fv(uTextureMatrix, 1, false, transformMatrix, 0)
 
-        //simple
-//        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, frameBuffer!!)
-//        GLES20.glActiveTexture(GLES20.GL_TEXTURE0)
-//        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, inputTextureId)
-//        GLES20.glUniform1i(uTextureLocation, 0)
-//        GLES20.glUniformMatrix4fv(uTextureMatrix, 1, false, transformMatrix, 0)
-//
-//        if (null != buffer) {
-//            buffer!!.position(0)
-//            GLES20.glEnableVertexAttribArray(aPositionLocation)
-//            GLES20.glVertexAttribPointer(aPositionLocation, 2, GLES20.GL_FLOAT, false, 16, buffer)
-//
-//            buffer!!.position(2)
-//            GLES20.glEnableVertexAttribArray(aTextureCoordinateLocation)
-//            GLES20.glVertexAttribPointer(aTextureCoordinateLocation, 2, GLES20.GL_FLOAT, false, 16, buffer)
-//
-//            GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 6)
-//        }
+        drawer.draw()
+
+        GLES20.glFinish()
+        GLES20.glDisableVertexAttribArray(aPositionLocation)
+        GLES20.glDisableVertexAttribArray(aTextureCoordinateLocation)
+        GLES20.glBindTexture(GLES11Ext.GL_TEXTURE_EXTERNAL_OES, GLES20.GL_NONE)
+        GLES20.glUseProgram(GLES20.GL_NONE)
+        GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, GLES20.GL_NONE)
+    }
+
+    private fun getVertices(cropRatioWidth: Float, cropRatioHeight: Float): FloatArray {
+        val x = if (cropRatioWidth > 1) 1f else cropRatioWidth
+        val y = if (cropRatioHeight > 1) 1f else cropRatioHeight
+        val left = (1 - x) / 2
+        var right = left + x
+        val bottom = (1 - y) / 2
+        val top = bottom + y
+        debug_e("crop($left, $top, $right, $bottom)")
+        return floatArrayOf(
+                left, bottom,//LEFT,BOTTOM
+                right, bottom,//RIGHT,BOTTOM
+                left, top,//LEFT,TOP
+                right, top//RIGHT,TOP
+        )
     }
 }
