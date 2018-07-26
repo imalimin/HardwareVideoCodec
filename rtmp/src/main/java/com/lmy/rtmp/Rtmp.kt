6c12d403cb01d4a734119703b31ccc892a3862ba
limin <lmyooyo@gmail.com>
2018-07-26 22:44:38 +0800

    分离连接和连接流逻辑

--- rtmp/src/main/java/com/lmy/rtmp/Rtmp.kt
+++ rtmp/src/main/java/com/lmy/rtmp/Rtmp.kt
@@ -1,8 +1,10 @@
 package com.lmy.rtmp
 
 interface Rtmp {
-    fun init()
-    fun connect(url: String, width: Int, height: Int, timeOut: Int): Int
+    //连接RTMP服务器
+    fun connect(url: String, timeOut: Int): Int
+    //连接成功后默认打开stream
+    fun connectStream(width: Int, height: Int): Int
     fun sendVideoSpecificData(sps: ByteArray, spsLen: Int, pps: ByteArray, ppsLen: Int, timestamp: Long): Int
     fun sendVideo(data: ByteArray, len: Int, timestamp: Long): Int
     fun sendAudioSpecificData(data: ByteArray, len: Int): Int
--- rtmp/src/main/java/com/lmy/rtmp/RtmpClient.kt
+++ rtmp/src/main/java/com/lmy/rtmp/RtmpClient.kt
@@ -4,13 +4,13 @@ package com.lmy.rtmp
  * Created by lmyooyo@gmail.com on 2018/7/25.
  */
 class RtmpClient : Rtmp {
+
     init {
         System.loadLibrary("rtmp")
-        init()
     }
 
-    external override fun init()
-    external override fun connect(url: String, width: Int, height: Int, timeOut: Int): Int
+    external override fun connect(url: String, timeOut: Int): Int
+    external override fun connectStream(width: Int, height: Int): Int
     external override fun sendVideoSpecificData(sps: ByteArray, spsLen: Int, pps: ByteArray, ppsLen: Int, timestamp: Long): Int
     external override fun sendVideo(data: ByteArray, len: Int, timestamp: Long): Int
     external override fun sendAudioSpecificData(data: ByteArray, len: Int): Int
