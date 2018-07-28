1c969327a8ad2ad1e0f3e2bbbaf0772de7231ad9
limin <lmyooyo@gmail.com>
2018-07-28 22:21:37 +0800

    完善脚本

--- codec/src/main/jni/glhelper/Android.mk
+++ codec/src/main/jni/glhelper/Android.mk
@@ -11,7 +11,6 @@ LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/*.cpp)
 # 打印引入的C文件列表
 $(warning $(LOCAL_SRC_FILES))
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_CFLAGS += -DNO_CRYPTO
 LOCAL_MODULE := libglhelper
 LOCAL_LDLIBS := -llog -lGLESv2
