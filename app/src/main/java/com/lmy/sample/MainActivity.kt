d4aa4df4f0d1ade8e3f37829c5c2a01298f35085
limin <lmyooyo@gmail.com>
2018-08-01 23:11:19 +0800

    更换ip

--- app/src/main/java/com/lmy/sample/MainActivity.kt
+++ app/src/main/java/com/lmy/sample/MainActivity.kt
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity(), View.OnTouchListener, RadioGroup.OnChe
             return
         val context = CodecContext(GLHelper.isSupportPBO(this))
 //        context.ioContext.path = "${Environment.getExternalStorageDirectory().absolutePath}/test.mp4"
-        context.ioContext.path = "rtmp://192.168.16.124:1935/live/livestream"
+        context.ioContext.path = "rtmp://192.168.16.203:1935/live/livestream"
         mPresenter = RecordPresenter(context)
         mPresenter.setOnStateListener(onStateListener)
         defaultVideoWidth = mPresenter.context.video.width
