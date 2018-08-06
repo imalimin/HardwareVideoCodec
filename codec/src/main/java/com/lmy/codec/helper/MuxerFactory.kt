83c8700e32b0179bf3362b04d86469ace50ec723
李明艺 <lmyooyo@gmail.com>
2018-08-06 17:38:40 +0800

    删除日志

--- codec/src/main/java/com/lmy/codec/helper/MuxerFactory.kt
+++ codec/src/main/java/com/lmy/codec/helper/MuxerFactory.kt
@@ -3,9 +3,8 @@ package com.lmy.codec.helper
 import android.text.TextUtils
 import com.lmy.codec.entity.CodecContext
 import com.lmy.codec.muxer.Muxer
-import com.lmy.codec.muxer.impl.RtmpMuxerImpl
 import com.lmy.codec.muxer.impl.MuxerImpl
-import com.lmy.codec.util.debug_i
+import com.lmy.codec.muxer.impl.RtmpMuxerImpl
 
 /**
  * Created by lmyooyo@gmail.com on 2018/7/25.
@@ -16,7 +15,6 @@ class MuxerFactory {
             if (TextUtils.isEmpty(context.ioContext.path)) {
                 throw RuntimeException("context.ioContext.path can not be null!")
             }
-            debug_i("Open muxer for ${context.ioContext.path}")
             return if (context.ioContext.path!!.startsWith("rtmp")) {
                 RtmpMuxerImpl(context)
             } else {
