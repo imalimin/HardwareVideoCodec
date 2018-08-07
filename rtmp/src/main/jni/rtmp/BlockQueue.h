262d39558ca17c18a3ce92c1c6da080a430d59ea
李明艺 <lmyooyo@gmail.com>
2018-08-07 17:32:47 +0800

    删除无用头

--- rtmp/src/main/jni/rtmp/BlockQueue.h
+++ rtmp/src/main/jni/rtmp/BlockQueue.h
@@ -4,10 +4,9 @@
  * This source code is licensed under the GPL license found in the
  * LICENSE file in the root directory of this source tree.
  */
-#include "../../../../../codec/src/main/jni/codec/log.h"
+#include "log.h"
 #include <string.h>
 #include <pthread.h>
-#include <queue>
 #include <list>
 
 using namespace std;
