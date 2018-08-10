2dc11a21c81a52c7691457d3421828424ec97a2d
limin <lmyooyo@gmail.com>
2018-08-11 00:17:07 +0800

    修复语法问题

--- codec/src/main/assets/shader/fragment_rgb2yuv.sh
+++ codec/src/main/assets/shader/fragment_rgb2yuv.sh
@@ -4,7 +4,7 @@ uniform sampler2D uTexture;
 void main(){
     vec4 color = texture2D(uTexture, vTextureCoord);
     vec3 rgb = vec3(color.r, color.g, color.b);
-    yuv = mat3(0.299,   0.587,   0.114,
+    vec3 yuv = mat3(0.299,   0.587,   0.114,
                -0.1678, -0.3313, 0.5,
                0.5,     -0.4187, -0.0813) * rgb;
     gl_FragColor = vec4(yuv, 1);
