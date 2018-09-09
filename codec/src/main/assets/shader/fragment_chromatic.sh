c737c7a9c664e1f63006de634553ad2c3bdf195a
limin <lmyooyo@gmail.com>
2018-09-09 21:33:41 +0800

    优化glsl

--- codec/src/main/assets/shader/fragment_chromatic.sh
+++ codec/src/main/assets/shader/fragment_chromatic.sh
@@ -4,12 +4,11 @@ uniform float time;
 uniform sampler2D sTexture;
 varying vec2 vTextureCoord;
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord )
-{
-    vec2 uv = fragCoord.xy;
+void main() {
+    vec2 uv = vTextureCoord.xy;
 
 	float amount = 0.0;
-	
+
 	amount = (1.0 + sin(time*6.0)) * 0.5;
 	amount *= 1.0 + sin(time*16.0) * 0.5;
 	amount *= 1.0 + sin(time*19.0) * 0.5;
@@ -17,18 +16,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 	amount = pow(amount, 3.0);
 
 	amount *= 0.05;
-	
+
     vec3 col;
     col.r = texture2D( sTexture, vec2(uv.x+amount,uv.y) ).r;
     col.g = texture2D( sTexture, uv ).g;
     col.b = texture2D( sTexture, vec2(uv.x-amount,uv.y) ).b;
 
 	col *= (1.0 - amount * 0.5);
-	
-    fragColor = vec4(col,1.0);
-}
 
-
-void main() {
-	mainImage(gl_FragColor, vTextureCoord);
+    gl_FragColor = vec4(col,1.0);
 }
