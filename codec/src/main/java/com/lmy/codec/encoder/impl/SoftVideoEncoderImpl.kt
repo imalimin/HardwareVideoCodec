4a06618eee2c18ce7081f155de290d098f22b4da
李明艺 <lmyooyo@gmail.com>
2018-09-13 16:04:36 +0800

    删除日志

--- codec/src/main/java/com/lmy/codec/encoder/impl/SoftVideoEncoderImpl.kt
+++ codec/src/main/java/com/lmy/codec/encoder/impl/SoftVideoEncoderImpl.kt
@@ -129,9 +129,7 @@ class SoftVideoEncoderImpl(var context: CodecContext,
     override fun onFrameAvailable(surfaceTexture: SurfaceTexture?) {
         synchronized(mEncodingSyn) {
             if (mEncoding && inited) {
-                val ttt = System.currentTimeMillis()
                 readPixels()
-                debug_e("readPixels cost " + (System.currentTimeMillis() - ttt))
                 mPipeline.queueEvent(Runnable { encode() })
                 reader?.recycleBuffer()
             }
