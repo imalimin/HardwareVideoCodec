54b2ea510bbd97827bcece1a2951aabccf7dc560
李明艺 <lmyooyo@gmail.com>
2018-09-14 17:58:34 +0800

    完善脚本

--- codec/src/main/jni/glhelper/Android.mk
+++ codec/src/main/jni/glhelper/Android.mk
@@ -11,8 +11,18 @@ LOCAL_SRC_FILES += $(wildcard $(LOCAL_PATH)/*.cpp)
 # 打印引入的C文件列表
 $(warning $(LOCAL_SRC_FILES))
 
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+LOCAL_CFLAGS := -D__cpusplus -g -mfloat-abi=softfp -mfpu=neon -march=armv7-a -mtune=cortex-a8 -DHAVE_NEON=1
+endif
+ifeq ($(TARGET_ARCH_ABI),$(filter $(TARGET_ARCH_ABI), armeabi-v7a x86))
+LOCAL_ARM_NEON := true
+endif
+
 LOCAL_CFLAGS += -DNO_CRYPTO
 LOCAL_MODULE := libglhelper
 LOCAL_LDLIBS := -llog -lGLESv2
+LOCAL_STATIC_LIBRARIES := cpufeatures
+
 include $(BUILD_SHARED_LIBRARY)
+$(call import-module,android/cpufeatures)
 ######## END glhelper ############
