1e417da34706a895912e8573824bab6e6ea4a218
limin <lmyooyo@gmail.com>
2018-09-15 16:37:35 +0800

    支持定义缓存大小，完善接口

--- codec/src/main/java/com/lmy/codec/x264/CacheX264Encoder.kt
+++ codec/src/main/java/com/lmy/codec/x264/CacheX264Encoder.kt
@@ -4,13 +4,14 @@ import android.media.MediaCodec
 import android.media.MediaFormat
 import com.lmy.codec.entity.Egl
 import com.lmy.codec.entity.RecycleQueue
-import com.lmy.codec.helper.GLHelper
+import com.lmy.codec.helper.Libyuv
 import java.nio.ByteBuffer
 
 /**
  * Created by lmyooyo@gmail.com on 2018/6/7.
  */
 class CacheX264Encoder(private val codec: X264Encoder,
+                       val maxCache: Int = 5,
                        private var cache: Cache? = null,
                        var onSampleListener: OnSampleListener? = null,
                        private var running: Boolean = true) : X264, Runnable {
@@ -18,32 +19,28 @@ class CacheX264Encoder(private val codec: X264Encoder,
     private var mEncodeThread = Thread(this).apply { name = "CacheX264Encoder" }
 
     init {
-        cache = Cache(codec.getWidth() * codec.getHeight() * Egl.COLOR_CHANNELS, 5)
+        var size = codec.getWidth() * codec.getHeight() * Egl.COLOR_CHANNELS
+        if (Libyuv.COLOR_I420 == codec.colorFormat) {
+            size = codec.getWidth() * codec.getHeight() * 3 / 2
+        }
+        cache = Cache(size, maxCache)
         codec.start()
         mEncodeThread.start()
     }
 
     fun encode(buffer: ByteBuffer) {
-        encode(buffer, 0)
-    }
-
-    private var offset = 0
-    fun encode(buffer: ByteBuffer, rowPadding: Int) {
         val data = cache?.pollCache() ?: return
         buffer.rewind()
-        if (rowPadding <= 0) {
-            buffer.get(data)
-        } else {
-            offset = 0
-            val width = getWidth()
-            val height = getHeight()
-            GLHelper.memcpy(buffer, data, height, width * 4, rowPadding)
-        }
+        buffer.get(data)
         cache?.offer(data)
     }
 
     override fun encode(src: ByteArray): MediaCodec.BufferInfo? {
-        return codec.encode(src)
+        val data = cache?.pollCache() ?: return null
+        System.arraycopy(src, 0, data, 0, data.size)
+        cache?.offer(data)
+        return null
+
     }
 
     override fun stop() {
@@ -65,7 +62,7 @@ class CacheX264Encoder(private val codec: X264Encoder,
                 e.printStackTrace()
                 break
             }
-            val bufferInfo = encode(data) ?: continue
+            val bufferInfo = codec.encode(data) ?: continue
             cache!!.recycle(data)
             if (X264Encoder.BUFFER_FLAG_CODEC_CONFIG == bufferInfo.flags) {
                 onSampleListener?.onFormatChanged(codec.getOutFormat())
