a671ff527663e24de8ef0028aefaaa9c91676a72
李明艺 <lmyooyo@gmail.com>
2018-09-18 10:08:00 +0800

    增加Encoder Builder

--- codec/src/main/java/com/lmy/codec/encoder/Encoder.kt
+++ codec/src/main/java/com/lmy/codec/encoder/Encoder.kt
@@ -9,6 +9,11 @@ package com.lmy.codec.encoder
 import android.graphics.SurfaceTexture
 import android.media.MediaCodec
 import android.media.MediaFormat
+import android.opengl.EGLContext
+import com.lmy.codec.encoder.impl.SoftVideoEncoderImpl
+import com.lmy.codec.encoder.impl.SoftVideoEncoderV2Impl
+import com.lmy.codec.encoder.impl.VideoEncoderImpl
+import com.lmy.codec.entity.CodecContext
 import java.nio.ByteBuffer
 
 /**
@@ -37,5 +42,23 @@ interface Encoder : SurfaceTexture.OnFrameAvailableListener {
 
     interface OnRecordListener {
         fun onRecord(encoder: Encoder, timeUs: Long)
+    }
+
+    class Builder(private val context: CodecContext,
+                  private val textureId: IntArray,
+                  private val eglContext: EGLContext,
+                  private var onPreparedListener: OnPreparedListener? = null) {
+        fun build(): Encoder {
+            return if (CodecContext.CodecType.HARD == context.codecType) {
+                VideoEncoderImpl(context, textureId, eglContext, onPreparedListener)
+            } else {
+                SoftVideoEncoderImpl(context, textureId, eglContext, onPreparedListener)
+            }
+        }
+
+        fun setOnPreparedListener(listener: OnPreparedListener): Builder {
+            onPreparedListener = listener
+            return this
+        }
     }
 }
