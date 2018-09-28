035a8d28aa88413fd87b779f9013b81ba2fc2f6d
limin <lmyooyo@gmail.com>
2018-09-28 23:57:34 +0800

    文字贴纸

---
+++ codec/src/main/java/com/lmy/codec/texture/impl/sticker/TextSticker.kt
@@ -0,0 +1,8 @@
+package com.lmy.codec.texture.impl.sticker
+
+class TextSticker(frameBuffer: IntArray,
+                  name: String = "BaseSticker") : BaseSticker(frameBuffer, name) {
+    override fun draw(transformMatrix: FloatArray?) {
+
+    }
+}
