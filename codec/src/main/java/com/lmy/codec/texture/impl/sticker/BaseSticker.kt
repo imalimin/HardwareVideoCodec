5ed525969eb1147b9f7a0783e58464506150fe11
limin <lmyooyo@gmail.com>
2018-09-28 23:57:25 +0800

    贴纸基础类

---
+++ codec/src/main/java/com/lmy/codec/texture/impl/sticker/BaseSticker.kt
@@ -0,0 +1,7 @@
+package com.lmy.codec.texture.impl.sticker
+
+import com.lmy.codec.texture.impl.BaseTexture
+
+abstract class BaseSticker(frameBuffer: IntArray,
+                           name: String = "BaseSticker") : BaseTexture(frameBuffer, name) {
+}
