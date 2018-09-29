35c9150c43b23f2b34b5160bfbfc05fff44f2e8a
李明艺 <lmyooyo@gmail.com>
2018-09-29 19:31:53 +0800

    完善逻辑

--- codec/src/main/java/com/lmy/codec/texture/impl/sticker/BaseSticker.kt
+++ codec/src/main/java/com/lmy/codec/texture/impl/sticker/BaseSticker.kt
@@ -1,6 +1,8 @@
 package com.lmy.codec.texture.impl.sticker
 
+import android.graphics.Bitmap
 import android.opengl.GLES20
+import android.opengl.GLUtils
 import com.lmy.codec.helper.Resources
 import com.lmy.codec.texture.impl.BaseTexture
 import javax.microedition.khronos.opengles.GL10
@@ -9,15 +11,14 @@ abstract class BaseSticker(var frameBuffer: IntArray,
                            var width: Int,
                            var height: Int,
                            name: String = "BaseSticker") : BaseTexture(frameBuffer, name) {
-
-    private var texture: IntArray = IntArray(1)
     private var aPositionLocation = 0
     private var aTextureCoordinateLocation = 0
     private var uTextureLocation = 0
+    private var texture: IntArray = IntArray(1)
 
-    init {
+    open fun init() {
         createProgram()
-        createTexture()
+        createTexture(texture)
     }
 
     private fun createProgram() {
@@ -28,7 +29,7 @@ abstract class BaseSticker(var frameBuffer: IntArray,
         aTextureCoordinateLocation = getAttribLocation("aTextureCoord")
     }
 
-    private fun createTexture() {
+    fun createTexture(texture: IntArray) {
         GLES20.glGenTextures(texture.size, texture, 0)
         GLES20.glBindTexture(GL10.GL_TEXTURE_2D, texture[0])
         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
@@ -39,6 +40,12 @@ abstract class BaseSticker(var frameBuffer: IntArray,
                 GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE.toFloat())
         GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
                 GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE.toFloat())
+        GLES20.glBindTexture(GL10.GL_TEXTURE_2D, GLES20.GL_NONE)
+    }
+
+    fun bindTexture(bitmap: Bitmap) {
+        GLES20.glBindTexture(GL10.GL_TEXTURE_2D, texture[0])
+        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0)
         GLES20.glBindTexture(GL10.GL_TEXTURE_2D, GLES20.GL_NONE)
     }
 
@@ -55,5 +62,10 @@ abstract class BaseSticker(var frameBuffer: IntArray,
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, GLES20.GL_NONE)
         GLES20.glUseProgram(GLES20.GL_NONE)
         GLES20.glFlush()
+    }
+
+    override fun release() {
+        super.release()
+        GLES20.glDeleteTextures(texture.size, texture, 0)
     }
 }
--- codec/src/main/java/com/lmy/codec/texture/impl/sticker/TextSticker.kt
+++ codec/src/main/java/com/lmy/codec/texture/impl/sticker/TextSticker.kt
@@ -1,9 +1,59 @@
 package com.lmy.codec.texture.impl.sticker
 
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.text.TextUtils
+
 class TextSticker(frameBuffer: IntArray,
-                  name: String = "BaseSticker") : BaseSticker(frameBuffer, name) {
+                  width: Int,
+                  height: Int,
+                  name: String = "BaseSticker") : BaseSticker(frameBuffer, width, height, name) {
+    private var text: String = "HWVC"
+    private var paint: Paint = Paint()
+    private var bitmap: Bitmap? = null
+
+    init {
+        paint.strokeWidth = 23f
+        paint.color = Color.WHITE
+    }
+
+    fun setText(text: String, size: Float) {
+        this.text = text
+        paint.strokeWidth = size
+    }
+
+    private fun createBitmap(): Bitmap {
+        val size = measureTextSize(paint, text)
+        val bitmap = Bitmap.createBitmap(Math.ceil(size[0].toDouble()).toInt(),
+                Math.ceil(size[1].toDouble()).toInt(),
+                Bitmap.Config.ARGB_8888)
+        return bitmap
+    }
+
+    override fun init() {
+        super.init()
+    }
 
     override fun draw(transformMatrix: FloatArray?) {
+        if (null != bitmap && !bitmap!!.isRecycled) {
 
+        }
+    }
+
+    /**
+     * 测量文本宽高
+     *
+     * @param paint 画笔
+     * @param text  文本
+     * @return 宽高
+     */
+    fun measureTextSize(paint: Paint, text: String): FloatArray {
+        if (TextUtils.isEmpty(text)) return floatArrayOf(0f, 0f)
+        val width = paint.measureText(text, 0, text.length)
+        val bounds = Rect()
+        paint.getTextBounds(text, 0, text.length, bounds)
+        return floatArrayOf(width, bounds.height().toFloat())
     }
 }
