b96611cc50b4c0019351508e8f742cf120145a90
limin <lmyooyo@gmail.com>
2018-10-03 02:04:44 +0800

    完善逻辑

--- codec/src/main/java/com/lmy/codec/decoder/impl/HardVideoDecoderImpl.kt
+++ codec/src/main/java/com/lmy/codec/decoder/impl/HardVideoDecoderImpl.kt
@@ -7,23 +7,25 @@ import android.media.MediaFormat
 import android.os.Build
 import android.view.Surface
 import com.lmy.codec.decoder.Decoder
+import com.lmy.codec.entity.CodecContext
 import com.lmy.codec.entity.Track
 import com.lmy.codec.pipeline.Pipeline
-import com.lmy.codec.pipeline.impl.EventPipeline
 import com.lmy.codec.util.debug_e
 import com.lmy.codec.util.debug_i
+import com.lmy.codec.wrapper.CameraTextureWrapper
 import java.io.IOException
 
 
-class HardVideoDecoderImpl(val texture: SurfaceTexture,
+class HardVideoDecoderImpl(val context: CodecContext,
+                           private val pipeline: Pipeline,
                            private val forPlay: Boolean = false) : Decoder {
     override var onFrameAvailableListener: SurfaceTexture.OnFrameAvailableListener? = null
+    override var textureWrapper: CameraTextureWrapper? = null
     private var extractor: MediaExtractor? = null
     private var videoTrack: Track? = null
     private var audioTrack: Track? = null
     private var codec: MediaCodec? = null
     private var path: String? = null
-    private var pipeline: Pipeline? = EventPipeline.create("HVDecoderImpl")
     private var videoInfo: MediaCodec.BufferInfo = MediaCodec.BufferInfo()
     private var startting = false
     private var eos = false
@@ -38,7 +40,7 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
         lastPts = 0
     }
 
-    override fun prepare() {
+    private fun prepareExtractor() {
         pipeline?.queueEvent(Runnable {
             extractor = MediaExtractor()
             try {
@@ -48,6 +50,31 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
                 return@Runnable
             }
             videoTrack = Track.getVideoTrack(extractor!!)
+            context.video.width = getWidth()
+            context.video.height = getHeight()
+            context.cameraSize.width = context.video.width
+            context.cameraSize.height = context.video.height
+        })
+    }
+
+    private fun updateTexture() {
+        textureWrapper?.updateTexture()
+        textureWrapper?.updateLocation(context)
+        textureWrapper?.surfaceTexture!!.setOnFrameAvailableListener(onFrameAvailableListener)
+    }
+
+    private fun prepareWrapper() {
+        pipeline?.queueEvent(Runnable {
+            debug_i("prepareWrapper ${getWidth()}x${getHeight()}")
+            textureWrapper = CameraTextureWrapper(getWidth(), getHeight())
+            updateTexture()
+        })
+    }
+
+    override fun prepare() {
+        prepareExtractor()
+        prepareWrapper()
+        pipeline?.queueEvent(Runnable {
             if (null == videoTrack) {
                 debug_e("Video track not found")
                 return@Runnable
@@ -55,7 +82,7 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
             videoTrack!!.select(extractor!!)
             try {
                 codec = MediaCodec.createDecoderByType(videoTrack!!.format.getString(MediaFormat.KEY_MIME))
-                codec!!.configure(videoTrack!!.format, Surface(texture), null, 0)
+                codec!!.configure(videoTrack!!.format, Surface(textureWrapper!!.surfaceTexture), null, 0)
             } catch (e: IOException) {
                 debug_e("Cannot open decoder")
                 return@Runnable
@@ -73,6 +100,7 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
         } else 0
         pipeline?.queueEvent(Runnable {
             if (!startting) return@Runnable
+            textureWrapper?.egl?.makeCurrent()
             val index = codec!!.dequeueInputBuffer(WAIT_TIME)
             if (index >= 0) {
                 val buffer = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -94,6 +122,7 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
             } else {
                 debug_e("Cannot get input buffer!")
             }
+//            debug_i("next ${videoInfo.presentationTimeUs}")
             if (!eos) {
                 next()
             }
@@ -144,7 +173,10 @@ class HardVideoDecoderImpl(val texture: SurfaceTexture,
 
     override fun release() {
         stop()
-        pipeline?.quit()
+    }
+
+    override fun post(event: Runnable) {
+        pipeline.queueEvent(event)
     }
 
     override fun getWidth(): Int = if (null != videoTrack)
