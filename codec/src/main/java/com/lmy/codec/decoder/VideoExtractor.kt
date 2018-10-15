369b52e6480f1da6964d0b5c50fa2ed65abdb05b
李明艺 <lmyooyo@gmail.com>
2018-10-15 16:49:29 +0800

    视频解封装对象

---
+++ codec/src/main/java/com/lmy/codec/decoder/VideoExtractor.kt
@@ -0,0 +1,71 @@
+package com.lmy.codec.decoder
+
+import android.media.MediaExtractor
+import android.media.MediaFormat
+import com.lmy.codec.entity.CodecContext
+import com.lmy.codec.entity.Track
+import com.lmy.codec.util.debug_e
+import java.io.IOException
+
+/**
+ * Created by lmyooyo@gmail.com on 2018/10/15.
+ */
+class VideoExtractor(private val context: CodecContext,
+                     private val inputPath: String) {
+    private var extractor: MediaExtractor? = null
+    private var audioExtractor: MediaExtractor? = null
+    private var videoTrack: Track? = null
+    private var audioTrack: Track? = null
+
+    init {
+        prepareExtractor()
+    }
+
+    private fun prepareExtractor() {
+        extractor = MediaExtractor()
+        audioExtractor = MediaExtractor()
+        try {
+            extractor?.setDataSource(this.inputPath)
+            audioExtractor?.setDataSource(this.inputPath)
+        } catch (e: IOException) {
+            debug_e("File(${context.ioContext.path}) not found")
+            return
+        }
+        videoTrack = Track.getVideoTrack(extractor!!)
+        audioTrack = Track.getAudioTrack(audioExtractor!!)
+        context.orientation = if (videoTrack!!.format.containsKey(VideoDecoder.KEY_ROTATION))
+            videoTrack!!.format.getInteger(VideoDecoder.KEY_ROTATION) else 0
+        if (context.isHorizontal()) {
+            context.video.width = videoTrack!!.format.getInteger(MediaFormat.KEY_WIDTH)
+            context.video.height = videoTrack!!.format.getInteger(MediaFormat.KEY_HEIGHT)
+            context.cameraSize.width = context.video.width
+            context.cameraSize.height = context.video.height
+        } else {
+            context.video.width = videoTrack!!.format.getInteger(MediaFormat.KEY_HEIGHT)
+            context.video.height = videoTrack!!.format.getInteger(MediaFormat.KEY_WIDTH)
+            context.cameraSize.width = context.video.height
+            context.cameraSize.height = context.video.width
+        }
+    }
+
+    fun getVideoTrack(): Track? {
+        return videoTrack
+    }
+
+    fun getAudioTrack(): Track? {
+        return audioTrack
+    }
+
+    fun containAudio(): Boolean {
+        return null != audioTrack
+    }
+
+    fun release() {
+        extractor?.release()
+        extractor = null
+        audioExtractor?.release()
+        audioExtractor = null
+        videoTrack = null
+        audioTrack = null
+    }
+}
