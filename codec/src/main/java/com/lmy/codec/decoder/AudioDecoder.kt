f3c3bd0c134025ced9e294ddc9383c18aec3d353
李明艺 <lmyooyo@gmail.com>
2018-10-16 18:00:58 +0800

    增加开源协议描述

--- codec/src/main/java/com/lmy/codec/decoder/AudioDecoder.kt
+++ codec/src/main/java/com/lmy/codec/decoder/AudioDecoder.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018-present, lmyooyo@gmail.com.
+ *
+ * This source code is licensed under the GPL license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 package com.lmy.codec.decoder
 
 interface AudioDecoder : Decoder {
