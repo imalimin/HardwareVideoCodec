c627a5453449cbd71beeb4a8d48c56f3bf1c0461
李明艺 <lmyooyo@gmail.com>
2018-11-08 14:17:40 +0800

    增加拖动进度条操作

--- app/src/main/java/com/lmy/sample/VideoActivity.kt
+++ app/src/main/java/com/lmy/sample/VideoActivity.kt
@@ -7,7 +7,9 @@ import android.net.Uri
 import android.provider.MediaStore
 import android.support.v7.app.AlertDialog
 import android.text.TextUtils
+import android.view.MotionEvent
 import android.view.TextureView
+import android.view.View
 import android.widget.FrameLayout
 import android.widget.Toast
 import com.lmy.codec.presenter.VideoPlayer
@@ -17,12 +19,14 @@ import com.lmy.codec.presenter.impl.VideoProcessorImpl
 import kotlinx.android.synthetic.main.activity_image.*
 import java.io.File
 
-class VideoActivity : BaseActivity() {
+class VideoActivity : BaseActivity(), VideoPlayer.OnPlayStateListener, View.OnTouchListener {
 
     private var player: VideoPlayer? = null
     private var processor: VideoProcessor? = null
     private var mFilterController: FilterController? = null
     private var dialog: AlertDialog? = null
+    private var durationUs = 0L
+    private var requestSeek = false
     override fun getLayoutResource(): Int = R.layout.activity_image
     override fun initView() {
         var uri = intent.data
@@ -51,6 +55,7 @@ class VideoActivity : BaseActivity() {
             setPreviewDisplay(mTextureView)
             prepare()
         }
+        player?.onPlayStateListener = this
         player?.start()
         mFilterController = FilterController(player!!, progressLayout)
         mTextureContainer.setOnClickListener {
@@ -60,6 +65,10 @@ class VideoActivity : BaseActivity() {
                 player?.start()
         }
         initProcessor(path!!)
+        mNowTimeView.text = getTimeString(0)
+        mEndTimeView.text = getTimeString(0)
+        mProgressBar.isEnabled = false
+        mProgressBar.setOnTouchListener(this)
     }
 
     private fun initProcessor(path: String) {
@@ -87,6 +96,28 @@ class VideoActivity : BaseActivity() {
                 }
             })
         }
+    }
+
+    override fun onTouch(v: View, event: MotionEvent): Boolean {
+        when (v.id) {
+            mProgressBar.id ->
+                if (MotionEvent.ACTION_UP == event.action) {
+                    player?.seekTo((mProgressBar.progress * 1f / mProgressBar.max * durationUs).toLong())
+                    synchronized(this) {
+                        requestSeek = false
+                    }
+                } else if (MotionEvent.ACTION_DOWN == event.action) {
+                    synchronized(this) {
+                        requestSeek = true
+                    }
+                } else if (MotionEvent.ACTION_MOVE == event.action) {
+                    runOnUiThread {
+                        mNowTimeView.text = getTimeString((mProgressBar.progress * 1f
+                                / mProgressBar.max * durationUs).toLong())
+                    }
+                }
+        }
+        return false
     }
 
     private fun insert(path: String) {
@@ -136,5 +167,45 @@ class VideoActivity : BaseActivity() {
 
     private fun getOutputPath(input: String): String {
         return "${input.substring(0, input.lastIndexOf("."))}_filter.mp4"
+    }
+
+    private fun getTimeString(timeUs: Long): String {
+        if (timeUs <= 0) return "00:00"
+        val time = timeUs / 1000000
+        val s = addZero((time % 60).toString())
+        val m = addZero((time / 60).toString())
+        return "$m:$s"
+    }
+
+    private fun addZero(str: String): String {
+        if (str.isEmpty()) return "00"
+        if (1 == str.length) return "0$str"
+        return str;
+    }
+
+    override fun onPrepared(play: VideoPlayer, durationUs: Long) {
+        mProgressBar.isEnabled = true
+        this.durationUs = durationUs
+        runOnUiThread { mEndTimeView.text = getTimeString(durationUs) }
+        mProgressBar.progress = 0
+    }
+
+    override fun onStart(play: VideoPlayer) {
+    }
+
+    override fun onPause(play: VideoPlayer) {
+    }
+
+    override fun onStop(play: VideoPlayer) {
+    }
+
+    override fun onPlaying(play: VideoPlayer, timeUs: Long, durationUs: Long) {
+        synchronized(this) {
+            if (!requestSeek) {
+                val progress = (timeUs * 1000f / durationUs).toInt()
+                mProgressBar.progress = if (progress > mProgressBar.max) mProgressBar.max else progress
+                runOnUiThread { mNowTimeView.text = getTimeString(timeUs) }
+            }
+        }
     }
 }
