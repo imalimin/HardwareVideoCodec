abf3c8111a43900bf0b0890f612781c19677f0ca
李明艺 <lmyooyo@gmail.com>
2018-11-15 11:12:42 +0800

    修复shader语法错误

--- codec/src/main/assets/shader/vertex_beauty_v5.glsl
+++ codec/src/main/assets/shader/vertex_beauty_v5.glsl
@@ -4,10 +4,8 @@ attribute vec2 aTextureCoord;
 varying vec2 vTextureCoord;
 varying vec4 vBlurTextureCoord[SHIFT_SIZE];
 
-// 高斯算子左右偏移值，当偏移值为5时，高斯算子为 11 x 11
-
-const highp float texelWidthOffset = 5;
-const highp float texelHeightOffset = 5;
+const highp float texelWidthOffset = 1.0;
+const highp float texelHeightOffset = 1.0;
 
 void main(){
     gl_Position = aPosition;
@@ -15,6 +13,6 @@ void main(){
     vec2 singleStepOffset = vec2(texelWidthOffset, texelHeightOffset);
     for (int i = 0; i < SHIFT_SIZE; i++) {
             vBlurTextureCoord[i] = vec4(vTextureCoord.xy - float(i + 1) * singleStepOffset,
-                                           vTextureCoord.xy + float(i + 1) * singleStepOffset;
+                                           vTextureCoord.xy + float(i + 1) * singleStepOffset);
     }
 }
