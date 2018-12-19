7cc8eb41ffbabec6c6aa737ddbf74fabbc1ff07e
limin <lmyooyo@gmail.com>
2018-12-20 00:29:52 +0800

    增加测试方法

--- codec_native/src/main/cpp/render/Test.cpp
+++ codec_native/src/main/cpp/render/Test.cpp
@@ -2,3 +2,23 @@
 // Created by lmy on 2018/12/19.
 //
 
+#include <jni.h>
+#include <log.h>
+#include "Render.h"
+
+Render *render = nullptr;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT void JNICALL Java_com_lmy_samplenative_MainActivity_addMessage
+        (JNIEnv *env, jobject thiz) {
+    if (nullptr == render) {
+        render = new Render();
+    }
+    render->post();
+}
+
+#ifdef __cplusplus
+}
+#endif
