5e8f700efce8d60616fc18093553227d3805b17f
李明艺 <lmyooyo@gmail.com>
2018-12-25 20:47:50 +0800

    完善测试代码

--- samplenative/src/main/java/com/lmy/samplenative/MainActivity.kt
+++ samplenative/src/main/java/com/lmy/samplenative/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.lmy.samplenative
 
+import android.view.SurfaceHolder
 import android.view.SurfaceView
 import android.widget.Button
+import com.lmy.samplenative.processor.PictureProcessor
 
 
 class MainActivity : BaseActivity() {
     private lateinit var addBtn: Button
     private lateinit var surfaceView: SurfaceView
+    private var processor: PictureProcessor? = PictureProcessor()
 
     override fun getLayoutResource(): Int = R.layout.activity_main
     override fun initView() {
@@ -15,6 +18,17 @@ class MainActivity : BaseActivity() {
         addBtn.setOnClickListener {
             addMessage()
         }
+        surfaceView.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(holder: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+                processor?.prepare(holder.surface)
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+            }
+
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+            }
+        })
     }
 
     external fun addMessage()
