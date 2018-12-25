fd8d870841ee6e53ef51409a6943f20d43cf666c
李明艺 <lmyooyo@gmail.com>
2018-12-25 18:38:50 +0800

    改用Surface

--- samplenative/src/main/java/com/lmy/samplenative/processor/PictureProcessor.kt
+++ samplenative/src/main/java/com/lmy/samplenative/processor/PictureProcessor.kt
@@ -1,6 +1,6 @@
 package com.lmy.samplenative.processor
 
-import android.graphics.SurfaceTexture
+import android.view.Surface
 
 class PictureProcessor {
     private var handler: Long = 0
@@ -9,10 +9,10 @@ class PictureProcessor {
         handler = create()
     }
 
-    fun prepare(surface: SurfaceTexture) {
+    fun prepare(surface: Surface) {
         prepare(handler, surface)
     }
 
     private external fun create(): Long
-    private external fun prepare(handler: Long, surface: SurfaceTexture)
+    private external fun prepare(handler: Long, surface: Surface)
 }
