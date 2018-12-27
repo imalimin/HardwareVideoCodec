8da11bb6c0bf65dde9926c10fef919c784ee55a8
李明艺 <lmyooyo@gmail.com>
2018-12-27 15:05:30 +0800

    独立文件工具

---
+++ codec_native/src/main/cpp/common/include/FileUtils.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2018-present, lmyooyo@gmail.com.
+ *
+ * This source code is licensed under the GPL license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#ifndef HARDWAREVIDEOCODEC_FILEUTILS_H
+#define HARDWAREVIDEOCODEC_FILEUTILS_H
+
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+unsigned long readFile(string file, uint8_t **buffer);
+
+class FileUtils {
+
+};
+
+
+#endif //HARDWAREVIDEOCODEC_FILEUTILS_H
---
+++ codec_native/src/main/cpp/common/utils/FileUtils.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2018-present, lmyooyo@gmail.com.
+ *
+ * This source code is licensed under the GPL license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#include "../include/FileUtils.h"
+
+unsigned long readFile(string path, uint8_t **buffer) {
+    ifstream infile;
+    infile.open(path.data());
+    if (!infile.is_open()) return 0;
+
+    infile.seekg(0, ios::end);
+    unsigned long length = static_cast<unsigned long>(infile.tellg());
+    infile.seekg(0, ios::beg);
+    *buffer = new uint8_t[length];
+    infile.read(reinterpret_cast<char *>(*buffer), length);
+    return length;
+}
