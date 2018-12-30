e09e8d1a49eefaa2b94b80adaacac4e4a69e1c16
limin <lmyooyo@gmail.com>
2018-12-30 22:21:13 +0800

    增加文件转cdata测试

--- samplenative/src/test/java/com/lmy/samplenative/ExampleUnitTest.kt
+++ samplenative/src/test/java/com/lmy/samplenative/ExampleUnitTest.kt
@@ -4,6 +4,12 @@ import com.lmy.hwvcnative.filter.PinkFilter
 import org.junit.Test
 
 import org.junit.Assert.*
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.nio.charset.Charset
+import java.util.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -18,5 +24,25 @@ class ExampleUnitTest {
         System.loadLibrary("hwvc_native")
         PinkFilter(arrayOf("sTexture2", "sTexture3"),
                 arrayOf("textures/pink.png", "textures/pink.png"))
+    }
+
+    @Test
+    fun testPNG2CDATA() {
+        val file = File("D:/clean.png")
+        if (file.exists()) {
+            val input = FileInputStream(file)
+            val output = FileOutputStream("${file.absolutePath}.xml")
+            output.write("<![CDATA[".toByteArray(Charset.forName("utf-8")))
+
+            val buf = ByteArray(input.available())
+            input.read(buf)
+            output.write(Base64.getEncoder().encode(buf))
+
+            output.write("]]>".toByteArray(Charset.forName("utf-8")))
+            input.close()
+            output.close()
+            return
+        }
+        throw IOException("File not found")
     }
 }
