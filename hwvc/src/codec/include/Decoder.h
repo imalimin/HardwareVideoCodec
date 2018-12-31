4a9b1274e0d8223548c52c7036164a3cfdaffec3
limin <lmyooyo@gmail.com>
2018-12-31 19:39:21 +0800

    增加codec模块

---
+++ hwvc/src/codec/include/Decoder.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2018-present, lmyooyo@gmail.com.
+ *
+ * This source code is licensed under the GPL license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+#ifndef HARDWAREVIDEOCODEC_DECODER_H
+#define HARDWAREVIDEOCODEC_DECODER_H
+
+
+class Decoder {
+
+};
+
+
+#endif //HARDWAREVIDEOCODEC_DECODER_H
