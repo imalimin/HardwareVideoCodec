194a04566ffb6693e8a8a22e9df536c1407725d3
李明艺 <lmyooyo@gmail.com>
2019-02-28 16:11:43 +0800

    完善消息类型定义

--- hwvc/src/render/include/Unit.h
+++ hwvc/src/render/include/Unit.h
@@ -13,24 +13,26 @@
 #include "Message.h"
 #include <map>
 
+#define KID(a, b, c, d) ((d) | ((c) << 8) | ((b) << 16) | ((unsigned)(a) << 24))
+
 using namespace std;
 
-static constexpr int EVENT_COMMON_RELEASE = 0x0010000;
-static constexpr int EVENT_COMMON_PREPARE = 0x0010001;
-static constexpr int EVENT_COMMON_INVALIDATE = 0x0010002;
+static constexpr int EVENT_COMMON_RELEASE = KID('C', 'O', 'M', 0x01);
+static constexpr int EVENT_COMMON_PREPARE = KID('C', 'O', 'M', 0x02);
+static constexpr int EVENT_COMMON_INVALIDATE = KID('C', 'O', 'M', 0x03);
 
-static constexpr int EVENT_IMAGE_SHOW = 0x0020000;
+static constexpr int EVENT_IMAGE_SHOW = KID('I', 'M', 'G', 0x01);
 
-static constexpr int EVENT_RENDER_FILTER = 0x0030000;// 绘制滤镜
-static constexpr int EVENT_RENDER_SET_FILTER = 0x0030001;// 更新滤镜
+static constexpr int EVENT_RENDER_FILTER = KID('R', 'N', 'D', 0x01);// 绘制滤镜
+static constexpr int EVENT_RENDER_SET_FILTER = KID('R', 'N', 'D', 0x02);// 更新滤镜
 
-static constexpr int EVENT_SCREEN_DRAW = 0x0040000;
+static constexpr int EVENT_SCREEN_DRAW = KID('S', 'C', 'R', 0x01);
 
-static constexpr int EVENT_VIDEO_START = 0x0050000;
-static constexpr int EVENT_VIDEO_PAUSE = 0x0050001;
-static constexpr int EVENT_VIDEO_SEEK = 0x0050002;
-static constexpr int EVENT_VIDEO_SET_SOURCE = 0x0050003;
-static constexpr int EVENT_VIDEO_LOOP = 0x0050004;
+static constexpr int EVENT_VIDEO_START = KID('V', 'D', 'O', 0x01);
+static constexpr int EVENT_VIDEO_PAUSE = KID('V', 'D', 'O', 0x02);
+static constexpr int EVENT_VIDEO_SEEK = KID('V', 'D', 'O', 0x03);
+static constexpr int EVENT_VIDEO_SET_SOURCE = KID('V', 'D', 'O', 0x04);
+static constexpr int EVENT_VIDEO_LOOP = KID('V', 'D', 'O', 0x05);
 
 typedef bool (Unit::*EventFunc)(Message *);
 
