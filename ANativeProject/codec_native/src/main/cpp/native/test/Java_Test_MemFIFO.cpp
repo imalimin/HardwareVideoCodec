89073e4ad0b3756f2632193cf4b4562903b99be2
李明艺 <lmyooyo@gmail.com>
2019-05-30 15:44:11 +0800

    完善测试逻辑

--- ANativeProject/codec_native/src/main/cpp/native/test/Java_Test_MemFIFO.cpp
+++ ANativeProject/codec_native/src/main/cpp/native/test/Java_Test_MemFIFO.cpp
@@ -11,10 +11,12 @@
 extern "C" {
 #endif
 
-#define MemFIFOTest_FRAME_SIZE 8192
+#define MemFIFOTest_FRAME_SIZE 8
 HwFIFOBuffer *fifo = nullptr;
 uint8_t *data = nullptr;
 int index = 'a';
+int pushCount = 0;
+int takeCount = 0;
 
 JNIEXPORT void JNICALL Java_com_lmy_hwvcnative_test_MemFIFOTest_push
         (JNIEnv *env, jobject thiz) {
@@ -26,7 +28,13 @@ JNIEXPORT void JNICALL Java_com_lmy_hwvcnative_test_MemFIFOTest_push
         index = 'a';
     }
     memset(data, index, MemFIFOTest_FRAME_SIZE);
-    Logcat::i("HWVC", "MemFIFOTest_push: %c", (char) data[0]);
+    ++pushCount;
+    Logcat::i("HWVC", "MemFIFOTest_push: %c,%c,%c,%c,%c,%c,%c,%c, %d",
+              (char) data[0], (char) data[1],
+              (char) data[2], (char) data[3],
+              (char) data[4], (char) data[5],
+              (char) data[6], (char) data[7],
+              pushCount);
     fifo->push(data, MemFIFOTest_FRAME_SIZE);
     ++index;
 }
@@ -35,9 +43,15 @@ JNIEXPORT void JNICALL Java_com_lmy_hwvcnative_test_MemFIFOTest_take
         (JNIEnv *env, jobject thiz) {
     if (fifo) {
         HwAbsFrame *frame = fifo->take(MemFIFOTest_FRAME_SIZE);
-        Logcat::i("HWVC", "MemFIFOTest_take: %c, %lld",
-                  (char) frame->getData()[0],
-                  frame->getDataSize());
+        if (frame) {
+            ++takeCount;
+            Logcat::i("HWVC", "MemFIFOTest_take: %c,%c,%c,%c,%c,%c,%c,%c, %lld, %d",
+                      (char) frame->getData()[0], (char) frame->getData()[1],
+                      (char) frame->getData()[2], (char) frame->getData()[3],
+                      (char) frame->getData()[4], (char) frame->getData()[5],
+                      (char) frame->getData()[6], (char) frame->getData()[7],
+                      frame->getDataSize(), takeCount);
+        }
     }
 }
 
--- ANativeProject/samplenative/src/main/java/com/lmy/samplenative/TestMemFIFOActivity.kt
+++ ANativeProject/samplenative/src/main/java/com/lmy/samplenative/TestMemFIFOActivity.kt
@@ -6,21 +6,46 @@ import kotlinx.android.synthetic.main.activity_test_mem_fifo.*
 class TestMemFIFOActivity : BaseActivity() {
     private val tester = MemFIFOTest()
     private var count = 0
+    private var thread1: Thread? = null
+    private var thread2: Thread? = null
     override fun getLayoutResource(): Int = R.layout.activity_test_mem_fifo
 
     override fun initView() {
         textView.text
         linearLayout.setOnClickListener {
-            tester.push()
-            ++count
-            if (count >= 8) {
-                tester.take()
+            thread1 = object : Thread() {
+                override fun run() {
+                    while (true) {
+                        if (interrupted()) {
+                            break
+                        }
+                        tester.push()
+                        sleep(21)
+                    }
+                }
             }
+            thread1?.start()
+            thread2 = object : Thread() {
+                override fun run() {
+                    while (true) {
+                        if (interrupted()) {
+                            break
+                        }
+                        tester.take()
+                        sleep(21)
+                    }
+                }
+            }
+            thread2?.start()
         }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        thread1?.interrupt()
+        thread2?.interrupt()
+        thread1?.join()
+        thread2?.join()
         tester.release()
     }
 }
